4bd83d55ff46fafe5d80e0b34f7cd1f0c6a64343
gabrielhochmann <gabrielhochmannt@gmail.com>
2024-02-05 08:22:27 -0300

    Even or Odd - Beecrowd (1074)

---
+++ Beecrowd/1074.c
@@ -0,0 +1,52 @@
+/*
+ ============================================================================
+ Author      : Arkzesru;
+ Date        : February 04, 2024;
+ Name        : Even or Odd;
+ Source      : Beecrowd;
+ Description : Determining wheter a given number is even, odd, positve or
+               negative.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+void printSquareEvenNumbers(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int X;
+        scanf("%d", &X);
+
+        if (X == 0)
+        {
+            printf("NULL\n");
+        }
+        else if (X < 0 && X % 2 == 0)
+        {
+            printf("EVEN NEGATIVE\n");
+        }
+        else if (X < 0 && X % 2 != 0)
+        {
+            printf("ODD NEGATIVE\n");
+        }
+        else if (X > 0 && X % 2 != 0)
+        {
+            printf("ODD POSITIVE\n");
+        }
+        else if (X > 0 && X % 2 == 0)
+        {
+            printf("EVEN POSITIVE\n");
+        }
+    }
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    printSquareEvenNumbers(N);
+
+    return 0;
+}
